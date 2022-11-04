a32138a2a2b874f194f637cab1165247d5c471dd
subhashiscse <smollickcseiu@gmail.com>
2022-11-05 01:08:38 +0600

    Added Code

---
+++ CPUTracking/Models/Create/CPUAdvisor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+namespace CPUTracking.Models.Create
+{
+    public class CPUAdvisor
+    {
+        public int ItemId { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Session { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? CompanyName { get; set; }
+        public string? Position { get; set; }
+    }
+}
+
---
+++ CPUTracking/Models/Create/CPUMember.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+namespace CPUTracking.Models.Create
+{
+    public class CPUMember
+    {
+        public int ItemId { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Session { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? CodeforcesId { get; set; }
+        public string? CodechefId { get; set; }
+        public string? AtCoderId { get; set; }
+        public string? LightOjId { get; set; }
+        public string? TophId { get; set; }
+    }
+}
+
