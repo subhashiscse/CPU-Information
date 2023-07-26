bce17be86232983441f161a3a04ea5ed793306d7
subhashismollick <smollickcseiu@gmail.com>
2023-07-26 19:11:26 +0600

    delete employee file

--- CPUTracking/Controllers/Employee.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CPUTracking.Controllers
-{
-    class Employee
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string City { get; set; }
-    }
-}
